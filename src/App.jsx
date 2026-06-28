1decb9a1b5c7819b165453527cb48becc2310a8b
Pizzadreams <retsim120@gmail.com>
2026-06-28 17:31:46 -0400

    install and configure tailwind.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 function App() {
   return (
-    <>
-      <h1>Photography Portfolio</h1>
-    </>
+    <main className="min-h-screen bg-zinc-900 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-white">
+        Photography Portfolio
+      </h1>
+    </main>
   );
 }
 
