d8784d3a8098e45492950752340bd61c99d58cf6
Pizzadreams <retsim120@gmail.com>
2026-07-01 19:22:12 -0400

    updates to contact page

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,7 +1,7 @@
 export const contact = {
     email: "photographer@email.com",
     phone: "+1 (123) 456-7890",
-    location: "Dinosaur, Colorado",
+    location: "Bainbridge Island, Washington",
     instagram: "https://instagram.com/",
     facebook: "https://facebook.com/",
 };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,32 +6,32 @@ import { contact } from "../data/contact";
 
 function Contact() {
     return (
-        <section className="bg-white py-24">
+        <section className="bg-gray-50 py-22">
             <Container>
 
                 {/* Heading */}
                 <div className="mx-auto max-w-3xl text-center">
 
-                    <h1 className="text-5xl font-bold">
+                    <h1 className="text-4xl font-bold">
                         Let's Connect!
                     </h1>
 
-                    <p className="mt-6 text-lg leading-8 text-gray-600">
+                    <p className="mx-auto mt-6 max-w-2xl text-lg leading-8 text-gray-600">
                         [Insert your Call-To-Action here]
                     </p>
 
                 </div>
 
                 {/* Contact Section */}
-                <div className="mt-20 grid gap-10 lg:grid-cols-2">
+                <div className="mt-20 grid gap-1 lg:grid-cols-2">
 
                     {/* Google Map */}
-                    <div className="overflow-hidden rounded-2xl shadow-lg">
+                    <div className="overflow-hidden rounded-3xl shadow-sm">
 
                         <iframe
-                            title="Dinosaur, Colorado"
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24364.209047416116!2d-109.0264656291155!3d40.241837173935764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8745a5fae709b0e9%3A0x2a9ade53888285be!2sDinosaur%2C%20CO%2081610!5e0!3m2!1sen!2sus!4v1782946000836!5m2!1sen!2sus"
-                            className="h-[420px] w-full"
+                            title="Bainbridge Island, Washington"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d172038.2243267011!2d-122.68370146654892!3d47.64367689058479!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54903c3256128b2b%3A0xdfa86644a0f0930e!2sBainbridge%20Island%2C%20WA!5e0!3m2!1sen!2sus!4v1782946882095!5m2!1sen!2sus"
+                            className="h-[420px] w-full rounded-3"
                             loading="lazy"
                             allowFullScreen
                             referrerPolicy="no-referrer-when-downgrade"
